8764da2b96ad4b6c90a648cafbbc5cce16cdbc1b
astinchoi <asciineo@gmail.com>
2018-07-10 08:37:21 +0900

    change directory name

---
+++ 1장_숫자/finding_gcd.py
@@ -0,0 +1,15 @@
+def finding_gcd(a, b):
+    ''' 두 수의 최대공약수를 반환한다. '''
+    while(b != 0):
+        result = b
+        a, b = b, a % b        
+    return result
+
+def test_finding_gcd():
+    number1 = 21
+    number2 = 12
+    assert(finding_gcd(number1, number2) == 3)
+    print('Tests passed!')
+
+if __name__ == '__main__':
+    test_finding_gcd()
