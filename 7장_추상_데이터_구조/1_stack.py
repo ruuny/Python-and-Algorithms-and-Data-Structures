efc00ff887d8de7633343e9401dfa30075d00290
astinchoi <asciineo@gmail.com>
2018-08-22 17:53:52 +0900

    stack

---
+++ 7장_추상_데이터_구조/1_stack.py
@@ -0,0 +1,42 @@
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return not bool(self.items)
+
+    def push(self, value):
+        self.items.append(value)
+
+    def pop(self):
+        value = self.items.pop()
+        if value:
+            return value
+        else:
+            print("Stack is empty.")
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+        else:
+            print("Stack is empty.")
+
+    def __repr__(self):
+        return f"{self.items}"
+
+
+if __name__ == '__main__':
+    stack = Stack()
+    print(f"스택이 비었나요? {stack.isEmpty()}")
+    print("스택에 0-9까지의 항목을 추가합니다.")
+    for i in range(10):
+        stack.push(i)
+    print(f"스택 크기: {stack.size()}")
+    print(f"peek: {stack.peek()}")
+    print(f"pop: {stack.pop()}")
+    print(f"peek: {stack.peek()}")
+    print(f"스택이 비었나요? {stack.isEmpty()}")
+    print(stack)
---
+++ 7장_추상_데이터_구조/2_stack_with_node.py
@@ -0,0 +1,59 @@
+class Node(object):
+    def __init__(self, value=None, pointer=None):
+        self.value = value
+        self.pointer = pointer
+
+
+class Stack(object):
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return not bool(self.head)
+
+    def push(self, item):
+        self.head = Node(item, self.head)
+
+    def pop(self):
+        if self.head:
+            node = self.head
+            self.head = node.pointer
+            return node.value
+        else:
+            print('Stack is empty.')
+
+    def peek(self):
+        if self.head:
+            return self.head.value
+        else:
+            print('Stack is empty.')
+
+    def size(self):
+        node = self.head
+        count = 0
+        while node:
+            count +=1
+            node = node.pointer
+        return count
+
+    def _printList(self):
+        node = self.head
+        while node:
+            print(node.value, end=' ')
+            node = node.pointer
+        print()
+
+
+if __name__ == '__main__':
+    stack = Stack()
+    print(f"스택이 비었나요? {stack.isEmpty()}")
+    print("스택에 0-9까지의 항목을 추가합니다.")
+    for i in range(10):
+        stack.push(i)
+    stack._printList()
+    print(f"스택 크기: {stack.size()}")
+    print(f"peek: {stack.peek()}")
+    print(f"pop: {stack.pop()}")
+    print(f"peek: {stack.peek()}")
+    print(f"스택이 비었나요? {stack.isEmpty()}")
+    stack._printList()
