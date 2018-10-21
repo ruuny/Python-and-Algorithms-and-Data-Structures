dff49b4db186f8e7e6f9fbe427210117162f0948
Astin CHOI <asciineo@gmail.com>
2018-10-21 11:28:13 +0900

    ch8,9 -ing

---
+++ 8장_점근적_분석/find_fibonacci_seq.py
@@ -0,0 +1,6 @@
+def find_fibonacci_seq_rec(n):
+    if n < 2: return n
+    return find_fibonacci_seq_rec(n - 1) + find_fibonacci_seq_rec(n - 2)
+
+# if __name__ == '__main__':
+#     print(find_fibonacci_seq_rec(7))
